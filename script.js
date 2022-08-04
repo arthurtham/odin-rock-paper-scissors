14bf22b7cab66403ed4d990aef696c04c3e318b2
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2022-08-04 06:46:33 -0700

    Added button UI with match history

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
@@ -14,34 +16,34 @@ function playRound(playerSelection, computerSelection) {
         || !choices.includes(computerSelection)
         )) {
             console.log("One of the choices is invalid; tie game.");
-            return 0;
+            return {status: 0, message: "One of the choices is invalid; tie game."};
         }
     if (playerSelection === computerSelection) {
         console.log("It's a tie!");
-        return 0;
+        return {status: 0, message: "It's a tie!"};
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             console.log("You lose! Paper beats Rock.");
-            return -1;
+            return {status: -1, message: "You lose! Paper beats Rock."};
         } else {
             console.log("You win! Rock beats Scissors.");
-            return 1;
+            return {status: 1, message: "You win! Rock beats Scissors."};
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
             console.log("You lose! Scissors beats Paper.");
-            return -1;
+            return {status: -1, message: "You lose! Scissors beats Paper."};
         } else {
             console.log("You win! Paper beats Rock.");
-            return 1;
+            return {status: 1, message: "You win! Paper beats Rock."};
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
             console.log("You lose! Rock beats Scissors.");
-            return -1;
+            return {status: -1, message: "You lose! Rock beats Scissors."};
         } else {
             console.log("You win! Scissors beats Paper.");
-            return 1;
+            return {status: 1, message: "You win! Scissors beats Paper."};
         }
     }
 }
@@ -74,5 +76,46 @@ function game() {
         console.log("The game is a tie!");
     }
 }
+//game();
 
-game();
+function sayHello(e) {
+    //alert("Hello, " + e);
+    console.log(e);
+    console.log(e.target.id);
+}
+
+function playGame(e) {
+    const playerSelection = e.target.id;
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    updateStatus(result);
+}
+
+function updateStatus(result) {
+
+    let resultText = result.message;
+
+    switch (result.status) {
+        case 1: playerScore++; break;
+        case -1: computerScore++; break;
+        default: break;
+    }
+
+    const status = document.getElementById("status");
+    status.innerHTML = "";
+    const statusUpdate = document.createElement("p");
+    statusUpdate.textContent = resultText;
+    status.appendChild(statusUpdate);
+
+    const historyList = document.getElementById("history-list");
+    const historyItem = document.createElement("li");
+    historyItem.textContent = resultText + " You: " + playerScore + " Computer: " + computerScore;
+    const oldHistoryItem = document.querySelector("#history-list li:first-child");
+    historyList.insertBefore(historyItem, oldHistoryItem);
+
+    console.log(status);
+    console.log(result);
+}
+
+const playerButtons = document.querySelectorAll('.button');
+playerButtons.forEach(button => button.addEventListener('click', playGame));
